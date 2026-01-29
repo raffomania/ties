6928a36384eb75db351482d8ac260890d3cee6b8
Rafael Epplée <hello@rafa.ee>
2026-01-29 12:58:34 +0100

    Move bookmarklet to index page

--- src/views/index.rs
+++ src/views/index.rs
@@ -1,8 +1,14 @@
 use htmf::prelude_inline::*;
+use url::Url;
 
 use super::layout;
 
-pub fn view(layout: &layout::Template) -> Element {
+pub struct Data<'a> {
+    pub layout: &'a layout::Template,
+    pub base_url: &'a Url,
+}
+
+pub fn view(data: &Data) -> Element {
     super::layout::layout(
         fragment([
             header(
@@ -36,23 +42,60 @@ pub fn view(layout: &layout::Template) -> Element {
                         ],
                         "Create a list",
                     ),
-                )
-                .with([li(
-                    (),
-                    a(
-                        [
-                            class(
-                                "block px-4 py-2 border rounded border-neutral-700 \
-                                 hover:bg-neutral-700",
-                            ),
-                            href("/profile"),
-                        ],
-                        "Install the bookmarklet",
-                    ),
-                )])])],
+                )])
+                .with(bookmarklet_section(data))],
             ),
             // TODO add social links here
         ]),
-        layout,
+        data.layout,
+    )
+}
+
+pub fn bookmarklet_section(data: &Data) -> Element {
+    fragment([
+        header(
+            class("pt-8"),
+            [h2(class("font-bold"), "Install Bookmarklet")],
+        ),
+        section(
+            class("py-2"),
+            [bookmarklet_help(), bookmarklet(data.base_url)],
+        ),
+    ])
+}
+
+fn bookmarklet_help() -> Element {
+    fragment([
+        p(
+            class("mb-2"),
+            "Click the bookmarklet on any website to add it as a bookmark in
+      ties!",
+        ),
+        p(
+            [class("mb-4")],
+            "To install, drag the following link to your bookmarks / favorites toolbar:",
+        ),
+    ])
+}
+
+fn bookmarklet(base_url: &Url) -> Element {
+    // window.open(
+    //   "{ base_url }/bookmarks/create?url="
+    //   +encodeURIComponent(window.location.href)
+    //   +"&title="
+    //   +encodeURIComponent(document.title)
+    // )
+    a(
+        [
+            class(
+                "text-center my-2 font-bold text-orange-200 border rounded py-2 px-16 cursor-grab",
+            ),
+            href(format!(
+                "javascript:(function()%7Bwindow.open(%0A%20%20%22{base_url}%2Fbookmarks%2Fcreate%\
+             3Furl%3D%22%0A%20%20%2BencodeURIComponent(window.location.href)%0A%20%20%2B%22%\
+             26title%3D%22%0A%20%20%2BencodeURIComponent(document.title)%0A)%7D)()",
+            )),
+        ],
+        "Add to ties",
     )
 }
