8ce57f707c1d55dcff291e2ee0fc1e1fc3618d62
Rafael Epplée <hello@rafa.ee>
2026-02-01 21:26:32 +0100

    Fix missing schemaname in query for wiping database

--- src/db/all.rs
+++ src/db/all.rs
@@ -5,8 +5,18 @@ use crate::response_error::ResponseResult;
 
 pub async fn wipe_all_data(tx: &mut AppTx) -> ResponseResult<()> {
     let tables = query!(
-        r#"select tablename as "tablename!" from pg_tables where schemaname in ('public', 'tower_sessions') and tablename != '_sqlx_migrations';"#
-    ).fetch_all(&mut **tx).await?.into_iter().map(|record|record.tablename).collect::<Vec<_>>().join(",");
+        r#"
+        select schemaname as "schemaname!", tablename as "tablename!"
+        from pg_tables
+        where schemaname in ('public', 'tower_sessions')
+        and tablename != '_sqlx_migrations';"#
+    )
+    .fetch_all(&mut **tx)
+    .await?
+    .into_iter()
+    .map(|record| format!("{}.{}", record.schemaname, record.tablename))
+    .collect::<Vec<_>>()
+    .join(",");
 
     query(&format!("truncate {tables}"))
         .execute(&mut **tx)
