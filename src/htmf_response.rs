0b2c9e648a6cc20ffb96d023423942db532afff6
Rafael Epplée <hello@rafa.ee>
2026-02-05 11:39:56 +0100

    Disable htmf formatter for now

--- src/htmf_response.rs
+++ src/htmf_response.rs
@@ -1,7 +1,4 @@
-use anyhow::anyhow;
 use axum::response::{Html, IntoResponse};
-
-use crate::response_error::ResponseError;
 
 pub struct HtmfResponse(pub htmf::element::Element);
 
@@ -13,15 +10,6 @@ impl From<htmf::element::Element> for HtmfResponse {
 
 impl IntoResponse for HtmfResponse {
     fn into_response(self) -> axum::response::Response {
-        if cfg!(not(debug_assertions)) {
-            return Html(self.0.to_html()).into_response();
-        }
-
-        let Ok(html) = self.0.to_html_pretty() else {
-            return ResponseError::Anyhow(anyhow!("Failed to serialize htmf element"))
-                .into_response();
-        };
-
-        Html(html).into_response()
+        Html(self.0.to_html()).into_response()
     }
 }
