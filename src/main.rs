23a468943ec50d07094f51489796ff5fdd01440f
Rafael Epplée <hello@rafa.ee>
2026-02-20 12:44:24 +0100

    Remove unused library to speed up linking time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![warn(clippy::pedantic)]
+#![expect(clippy::missing_errors_doc)]
+#![expect(clippy::redundant_closure_for_method_calls)]
+
+mod authentication;
+pub mod cli;
+mod db;
+mod extract;
+mod form_errors;
+mod forms;
+mod oidc;
+mod response_error;
+mod routes;
+pub mod server;
+mod views;
+
+mod date_time;
+mod federation;
+mod htmf_response;
+#[cfg(debug_assertions)]
+mod insert_demo_data;
+#[cfg(test)]
+mod tests;
+
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    ties::cli::run().await
+    cli::run().await
 }
